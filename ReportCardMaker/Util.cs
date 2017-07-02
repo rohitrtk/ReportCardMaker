c9e0e4790cd2498c84fd33bc7c21170c69ed4c9d
rohitrtk <rohit_rtk@hotmail.com>
2017-07-02 00:54:42 -0400

    Additions to the Util class
    + Static Constructor
    + Made variables readonly
    + Try & Catch to LoadTextTo method

--- ReportCardMaker/Util.cs
+++ ReportCardMaker/Util.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ReportCardMaker
@@ -8,31 +10,62 @@ namespace ReportCardMaker
         /// <summary>
         /// Name of the project
         /// </summary>
-        private const string _name = "Report Card Maker";
+        private static readonly string _name;
 
         /// <summary>
         /// Project Version
         /// </summary>
-        private const string _version = "v0.1";
+        private static readonly string _version;
 
         /// <summary>
         /// Save file location
         /// </summary>
-        /// TODO: Set up the save path in a settings file which can be altered for each user
-        private static string _savePath;
+        private static readonly string _savePath;
+
+        /// <summary>
+        /// Template locations
+        /// </summary>
+        private readonly static Dictionary<string, string> _templates;
 
         public static string Name => _name;
         public static string Version => _version;
+        public static string SavePath => _savePath;
+        public static Dictionary<string, string> Templates => _templates;
 
-#region LoadTextTo
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static Util()
+        {
+            _name = "Report Card Maker";
+            _version = "v0.1";
+            _savePath = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+
+            _templates = new Dictionary<string, string>
+            {
+                { "Skills", _savePath + "skills" }
+            };
+        }
+
+        #region LoadTextTo
         /// <summary>
         /// Loads text to an output string specified in the parameters
         /// </summary>
         /// <param name="file"></param>
         /// <param name="output"></param>
-        public static void LoadTextTo(string file, string output)
+        public static void LoadTextTo(string file, string[] output)
         {
-            var data = File.ReadAllText(file);
+            string[] data;
+            try
+            {
+                data = File.ReadAllLines(file);
+            } 
+            catch(Exception)
+            {
+                Console.WriteLine("An Error occured writing data from file to object");
+                data = null;
+            }
+
             output = data;
         }
 
@@ -41,13 +74,22 @@ namespace ReportCardMaker
         /// </summary>
         /// <param name="file"></param>
         /// <returns></returns>
-        public static string LoadTextTo(string file)
+        public static string[] LoadTextTo(string file)
         {
-            return File.ReadAllText(file);
+            try
+            {
+                return File.ReadAllLines(file);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An Error occured writing data from file to object");
+                return null;
+            }
         }
-#endregion
+        #endregion
+        #region SaveTextTo
         /// <summary>
-        /// Saves a string of text to a file specified in the parameters
+        /// Saves a string of text to a line in a file specified in the parameters
         /// </summary>
         /// <param name="file"></param>
         /// <param name="input"></param>
@@ -60,10 +102,34 @@ namespace ReportCardMaker
             var w = new StreamWriter(file, append: true);
 
             // Add the text in to the file
-            w.WriteLine(input + "\n");
+            w.WriteLine(input);
 
             // Close the stream writer
             w.Close();
         }
+
+        /// <summary>
+        /// Saves a string array of text to a file specified in the parameters
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="input"></param>
+        public static void SaveTextTo(string file, string[] input)
+        {
+            // If the file doesn't exist, create it at the save path
+            if (!File.Exists(file)) File.Create(_savePath);
+
+            // Create a new stream writer to append the text to the file
+            var w = new StreamWriter(file, append: true);
+
+            // Foreach line of text in input; write it to the specified file
+            foreach(var v in input)
+            {
+                w.WriteLine(v);
+            }
+
+            // Close the stream writer
+            w.Close();
+        }
+        #endregion
     }
 }
